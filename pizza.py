0b87bddd089055034515b909689416f6e8d15dfd
Carlos Santiago Yanzon <santiago.yanzon1999@gmail.com>
2019-02-19 15:53:20 -0300

    uncorte
    - como evaluar los demas cortes
    - sistema para reahacer la operacion 1 y otra vez

--- pizza.py
+++ pizza.py
@@ -5,7 +5,10 @@
 #
 tom = 0 #Tomatoes
 mus = 0 #Mushroom
+
 global matrix
+global row
+global column
 global h
 global l
 
@@ -16,10 +19,14 @@ class Cor:
         self.y = y
 
 def calculateBalance(t1, m1): 
-    if t1 >= m1:
-        balance = t1/m1
+    if t1 != 0 and m1 != 0:
+        if t1 >= m1:
+            balance = t1/m1
+        else:
+            balance = m1/t1
     else:
-        balance = m1/t1
+        return 0
+    
     return balance
 
 def updateBalance(t1,m1):
@@ -33,15 +40,26 @@ def calculateBalanceOverSlice(r1, r2, c1,c2): #r1 and c1 are the start and r2 an
     totalColumns = c2-c1
     tom = 0
     mus = 0
-    for x in totalRows:
-        for y in totalColumns:
+    for x in range(totalRows):
+        for y in range(totalColumns):
             if matrix[x+r1][y+c1]== "T":
                 tom += 1
             elif matrix[x+r1][y+c1] == "M":
                 mus += 1
     return calculateBalance(tom, mus)
 
-        
+#Esta funcion nos sirve para controlar si la ppieza es indivisiblle o no cumple las condiciones, es util o puede ser cortada
+def chequearSlice(r1,r2,c1,c2):
+    maxBalance = h/l
+    balanceSlice = calculateBalanceOverSlice(r1,r2,c1, c2)
+    if balanceSlice > maxBalance or balanceSlice == 0: #Si NO VA A EXISTIR DIVISION QUE LE VENGA BIEN
+        return -1
+    else:
+        if (r2-r1)*(c2-c1) > h*2: #So Podemos dividirla 
+            return 0
+        else:
+            return 1 #Si cumple con la minima
+
 #
 # F I L E - R E A D I N G
 #
@@ -83,21 +101,20 @@ print(matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3], matrix[1][4])
 print(matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3], matrix[2][4])
 
 
-#
-#
-#
+
+
 print("Filas:",len(matrix),"Columnas:",len(matrix[0]))
 
 size = len(matrix)*len(matrix[0])
 #balance = calculateBalanceOverSlice(r1,r2,c1,c2)
 
-class pieza:
-    def __init__(self, r1,r2,c1,c2, padre):
+class Pieza:
+    def __init__(self, r1,c1):
         self.r1 = r1 #r1 = start row from original matrix, r2 end row from original mtri
-        self.r2 = r2 
+        self.r2 = r1 + 1 
         self.c1 = c1 #Same from rows for columns
-        self.c2 = c2
-        self.padre = padre #Objeto pieza
+        self.c2 = c1 + 1
+        #self.padre = padre #Objeto pieza
         self.hijos = []
         
     def updateHijos(self, hijos):
@@ -105,28 +122,39 @@ class pieza:
         
     #La idea de esta funciones, por un lado encontrar un cuadrado que cumpla las cndiciones y x otro lado que chequee que los que queden tambien lo sean
     #Chequeear los otros puede entrar en otra funcion, para eso esta el tema de hijos
-    def rotarYReformar():
-        balancePieza = calculateBalanceOverSlice(self.r1, self.r2, self.c1, self.c2)
-        row = r2-r1
-        column = c2-c1
+    def rotarYReformar(self):
+        validez = chequearSlice(self.r1, self.r2, self.c1, self.c2)
+        sliceRow = self.r2-self.r1
+        sliceColumn = self.c2-self.c1
+        
         #Tenemos que ver como darle forma y reforma a las piezas
-        while row*column <= h*2 or balancePieza != 1:
-            
+        while sliceRow*sliceColumn <= h*2 and validez != 1:
+            if sliceColumn < h*2:
+                if sliceRow*sliceColumn < h*2 and sliceRow < row:
+                    self.r2 += 1
+                elif sliceColumn < column:
+                    self.r2 = self.r1 + 1
+                    self.c2 += 1
+                else: 
+                    break
+            validez = chequearSlice(self.r1, self.r2, self.c1, self.c2)
+            print("validez:", validez)
+            sliceRow = self.r2-self.r1
+            sliceColumn = self.c2-self.c1
+        
+        print("test")
+        if sliceRow*sliceColumn > h*2:
+            print("No pudimos cortar la pieza!")
+            #Aca tenemos que trabajar lo de volver al padre y eliminar esta pieza, reformular al padre
+        else:
+            print("r1 a r2:", self.r1,self.r2)
+            print("c1 a c2:", self.c1,self.c2)
             
         #Si pasa este limite y no la encontro, nose puede y debemos volver al padre y retroceder
-        
 
-#Esta funcion nos sirve para controlar si la ppieza es indivisiblle o no cumple las condiciones, es util o puede ser cortada
-def chequearSlice(r1,r2,c1,c2, h, l):
-    maxBalance = h/l
-    balanceSlice = calculateBalanceOverSlice(r1,r2,c1, c2)
-    if balanceSlice > maxBalance or balanceSlice == 0: #Si NO VA A EXISTIR DIVISION QUE LE VENGA BIEN
-        return -1
-    else:
-        if (r2-r1)*(c2-c1) <= h*2: #So Podemos dividirla 
-            return 0
-        else:
-            return 1 #Si cumple con la minima
+        
+slice1 = Pieza(0,0)
+slice1.rotarYReformar()
       
         
     
